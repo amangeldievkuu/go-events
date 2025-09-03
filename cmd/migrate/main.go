5f166577739caa87514cc83ae894f6ed2ceba20b
amangeldievkuu <amangeldievkuu@gmail.com>
2025-09-03 15:28:50 +0700

    add force, status cmd for migrations

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/sqlite"
 	"github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -41,15 +44,46 @@ func main() {
 		log.Fatal(err)
 	}
 
+	defer m.Close()
+
 	switch direction {
 	case "up":
 		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
 			log.Fatal(err)
 		}
+		fmt.Println("Applied up migrations (or no change).")
+
 	case "down":
 		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
 			log.Fatal(err)
 		}
+		fmt.Println("Applied down migrations (or no change).")
+
+	case "status":
+		version, dirty, err := m.Version()
+		if err != nil && err != migrate.ErrNilVersion {
+			log.Fatal(err)
+		}
+
+		if err == migrate.ErrNilVersion {
+			fmt.Println("No migrations have been applied yet. (version nill)")
+		} else {
+			fmt.Printf("Current migration version: %d, dirty: %v\n", version, dirty)
+		}
+
+	case "force":
+		if len(os.Args) < 3 {
+			log.Fatal("Please provide a version to force")
+		}
+		v, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatalf("Invalid version: %v", err)
+		}
+
+		if err := m.Force(v); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Forced migration version to %d\n", v)
 
 	default:
 		log.Fatal("Unknown direction. Use up or down")
