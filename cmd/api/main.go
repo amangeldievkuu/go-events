ebf6818c0736fe7639c94244694bf8ecfb2bb526
amangeldievkuu <amangeldievkuu@gmail.com>
2025-09-11 11:55:49 +0800

    application server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,38 @@
-package api
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/amangeldievkuu/go-rest-events/internal/database"
+	"github.com/amangeldievkuu/go-rest-events/internal/env"
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type application struct {
+	port      int
+	jwtSecret string
+	models    database.Models
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	models := database.NewModels(db)
+
+	app := &application{
+		port:      env.GetEnvInt("PORT", 4000),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "supersecretkey"),
+		models:    models,
+	}
+
+	if err := app.serve(); err != nil {
+		log.Fatal(err)
+	}
+}
