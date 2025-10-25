7a3aeb54ba64aae722d540a79553101854549d5b
amangeldievkuu <amangeldievkuu@gmail.com>
2025-10-25 16:19:47 +0800

    add authorization

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -3,9 +3,11 @@ package main
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/amangeldievkuu/go-rest-events/internal/database"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -13,6 +15,55 @@ type registerRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=6"`
 	Name     string `json:"name" binding:"required,min=2"`
+}
+
+type loginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type loginResponse struct {
+	Token string `json:"token"`
+}
+
+func (app *application) login(c *gin.Context) {
+	var auth loginRequest
+
+	if err := c.ShouldBindJSON(&auth); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	existingUser, err := app.models.Users.GetByEmail(auth.Email)
+	if existingUser == nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "There is no user with such email"})
+		return
+	}
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "something went wrong"})
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(existingUser.Password), []byte(auth.Password))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid email or password"})
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": existingUser.Id,
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(app.jwtSecret))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error generating token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, loginResponse{Token: tokenString})
+
 }
 
 func (app *application) registerUser(c *gin.Context) {
